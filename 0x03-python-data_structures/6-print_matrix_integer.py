e8b485e03cb4d5bc310834f4d53ae3b9c0c55d4f
Emmanuel <emmanuellawakin2005@gmail.com>
2022-12-06 01:15:35 +0100

    Fixed code

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     """A function that prints a matrix of integers"""
     for row in range(len(matrix)):
         for column in range(len(matrix[row])):
-            print("{:d}".format(matrix[row][column]), end=" ")
+            print("{:d}".format(matrix[row][column]), end="")
+            if column != (len(matrix[row]) - 1):
+                print(" ", end="")
         print("")
