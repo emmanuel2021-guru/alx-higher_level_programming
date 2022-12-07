5b4b284ad4b13d4d4f6638a1d1eaecc5cabef6a8
Emmanuel <emmanuellawakin2005@gmail.com>
2022-12-07 16:14:53 +0100

    Fixed code

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import add_0
+if __name__ == "__main__":
+    from add_0 import add
 
-a = 1
-b = 2
-print("{:s} + {:s} = {:s}".format(str(a), str(b), str(add_0.add(a, b))))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
