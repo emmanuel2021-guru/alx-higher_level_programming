986f836055eed8377c0302f2237286aedf7f1701
Emmanuel <emmanuellawakin2005@gmail.com>
2022-12-20 17:11:23 +0100

    Fixed code for advanced task 100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
     except ValueError as ve:
-        print(f"Exception: {ve}")
+        sys.stderr.write("Exception: {}\n".format(ve))
         return False
