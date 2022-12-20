59c749779f95e2d17c35cea87645cf3d770b5c6c
Emmanuel <emmanuellawakin2005@gmail.com>
2022-12-20 14:24:47 +0100

    Created file and completed task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    len_my_list = 0
+    count = 0
+    for i in my_list:
+        len_my_list += 1
+    for index in range(x):
+        try:
+            print("{:d}".format(my_list[index]), end="")
+            count += 1
+            if count == len_my_list:
+                break
+        except (ValueError, TypeError):
+            pass
+        except IndexError:
+            raise
+    print("")
+    return count
