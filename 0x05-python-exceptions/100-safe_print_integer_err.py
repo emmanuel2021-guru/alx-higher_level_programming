4fa0d7d7bfc670cf92240c45b022893d7db499b6
Emmanuel <emmanuellawakin2005@gmail.com>
2022-12-20 17:08:56 +0100

    Fixed code

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,8 +2,7 @@
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
+        return True
     except ValueError as ve:
         print(f"Exception: {ve}")
         return False
-    else:
-        return True
