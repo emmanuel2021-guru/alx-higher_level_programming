d0b26f2840fa37fd1b33bf6b6b3441ed81035ed1
Emmanuel <emmanuellawakin2005@gmail.com>
2022-12-21 18:14:54 +0100

    Created file and completed task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""Define a square"""
+
+
+class Square:
+    """ Represents a square """
+    def __init__(self, size=0):
+        """ Initializes a square instance
+            and checks for exceptions
+
+            Args:
+                size (int): the size of the square
+        """
+        try:
+            if size < 0:
+                raise ValueError
+            else:
+                self.__size = size
+        except TypeError:
+            print("size must be an integer")
+        except ValueError:
+            print("size must be >= 0")
