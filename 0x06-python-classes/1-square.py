199885b2e2b57b826099a73b1574991178bd3308
Emmanuel <emmanuellawakin2005@gmail.com>
2022-12-21 17:56:52 +0100

    Created file and completed task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Define a square"""
+
+
+class Square:
+    """ Represents a square """
+    def __init__(self, size):
+        """ Initializes a square instance 
+
+            Args:
+                size: the size of the square
+        """
+        self.__size = size
