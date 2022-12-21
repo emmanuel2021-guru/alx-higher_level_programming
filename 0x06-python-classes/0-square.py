356ac72b4b2b3d885691dd58632faa5fdded4f4a
Emmanuel <emmanuellawakin2005@gmail.com>
2022-12-21 17:32:27 +0100

    Fixed code

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 class Square:
-    """ An empty class that defines a Square """
+    """ An empty class that defines a Square
+
+        Attributes:
+            No attributes """
     pass
