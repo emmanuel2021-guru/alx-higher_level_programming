40c0d808ae0c9966bb82c30172fe071930b2b9a1
Emmanuel <emmanuellawakin2005@gmail.com>
2022-12-22 02:09:13 +0100

    Created file and completed task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+"""Define a square"""
+
+
+class Square:
+    """ Represents a square """
+    def __init__(self, size=0):
+        """ Initializes a square instance
+            and checks for exceptions
+
+            Args:
+                size (int): the size of the square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """ Getter method for getting the size
+            of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Setter method for setting the size
+            of the square
+
+            Args:
+                value (int): the size of the square
+        """
+        try:
+            if type(value) is not int:
+                raise TypeError
+            elif value < 0:
+                raise ValueError
+            else:
+                self.__size = value
+        except TypeError:
+            print("size must be an integer")
+        except ValueError:
+            print("size must be >= 0")
+    
+    def area(self):
+        """ Returns the area of a square """
+        return self.size ** 2
