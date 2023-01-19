f9944436f387ed570c8ecefab5b3db08b8fa6629
Emmanuel <emmanuellawakin2005@gmail.com>
2023-01-19 19:04:11 +0100

    Created file and completed task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Module containing function that returns the JSON
+   representation of an object
+"""
+import json
+
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object
+
+       Args:
+            my_obj: object whose JSON representation
+                    is to be returned
+    """
+    return json.dumps(my_obj)
