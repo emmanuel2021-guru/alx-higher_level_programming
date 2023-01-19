9a86c3b81a0907f46ef579c74a3edc4eea04ed5a
Emmanuel <emmanuellawakin2005@gmail.com>
2023-01-19 14:12:09 +0100

    Fixed code

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
 
 import json
-"""Defines a class Base"""
+
+"""Defines a base class"""
 
 
-class Base(object):
+class Base():
     """Represents the base class of all other classes in project"""
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initializes an instance of Base class with id"""
+        """Initializes an instance of Base class with id
+           
+           Args:
+               id (int): instance id
+        """
         if id is not None:
             self.id = id
         else:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
-
-    @staticmethod
+    @classmethod
     def to_json_string(list_dictionaries):
-        """Returns the JSON string representation of list_dictionaries"""
+        """Returns the JSON string representation of list_dictionaries
+           
+           Args:
+               list_dictionaries: a list of dictionaries
+        """
         if list_dictionaries is None:
             return "[]"
         else:
