1ee46960aa74d03ed21a70b7414c31b3221b3cdc
Emmanuel <emmanuellawakin2005@gmail.com>
2023-01-19 16:24:05 +0100

    Fixed documentation and pycodestyle errors

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
 
-from models.rectangle import Rectangle
-
 """Defines a Square"""
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     """Represents a square"""
     def __init__(self, size, x=0, y=0, id=None):
-        """Initializes a Square instance"""
+        """Initializes a Square instance
+
+           Args:
+               size (int): the width and height of the square
+               x (int): offset on the x-axis
+               y (int): offset on the y-axis
+               id (int): Base id
+        """
         Rectangle.__init__(self, width=size, height=size, x=x, y=y, id=id)
 
     def __str__(self):
         """Returns a string representation of the Square instance"""
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        return "[Square] ({}) {}/{} - {}"\
+            .format(self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
-        """Getter function for the size attribute"""
+        """Sets/Gets the size"""
         return self.width
 
     @size.setter
     def size(self, val):
-        """Setter function for the size attribute"""
         if type(val) is int and val > 0:
             self.width = val
             self.height = val
@@ -32,7 +38,15 @@ class Square(Rectangle):
             raise ValueError("width must be > 0")
 
     def update(self, *args, **kwargs):
-        """Assigns argument to each attribute"""
+        """Updates the square
+
+           Args:
+               *args: the list of arguments
+                      1st argument - id attribute
+                      2nd argument - size attribute
+                      3rd argument - x attribute
+                      4th argument - y attribute
+               **kwargs: double pointer to a dictionary; key/value"""
         if len(args) != 0:
             for i in range(len(args)):
                 if i == 0:
@@ -56,5 +70,6 @@ class Square(Rectangle):
 
     def to_dictionary(self):
         """Returns the dictionary representation of a Rectangle"""
-        square_dict = {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
+        square_dict = \
+            {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
         return square_dict
