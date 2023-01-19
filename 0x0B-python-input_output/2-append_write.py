c1dfc55cd05bb65e20685f437aae86b15cefdd26
Emmanuel <emmanuellawakin2005@gmail.com>
2023-01-19 18:54:11 +0100

    Created file and completed task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Module containing a function that appends a string
+   at the end of a text file and returns the number
+   of characters added
+"""
+
+
+def append_write(filename="", text=""):
+    """Appends a string at the end of a text file(utf-8) and
+       returns the number of characters added
+    """
+    with open(filename, "a", encoding="utf-8") as fn:
+        nchar = fn.write(text)
+        return nchar
