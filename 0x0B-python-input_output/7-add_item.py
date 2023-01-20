eef282bb7f4f845aa5d08892fff57e6376cbc8d5
Emmanuel <emmanuellawakin2005@gmail.com>
2023-01-20 01:47:08 +0100

    Fixed pycodestyle error

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,9 +4,9 @@
    a python list, and save them to a file
 """
 
+import sys
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-import sys
 
 arg_list = list()
 try:
