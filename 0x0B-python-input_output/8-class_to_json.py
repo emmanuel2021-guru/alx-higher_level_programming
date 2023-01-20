577fa79d43ecdd83e8bfe7d72a7e65e923269c5d
Emmanuel <emmanuellawakin2005@gmail.com>
2023-01-20 02:18:31 +0100

    Created file and completed task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Module containing a function that returns the dictionary
+   description with simple data structure
+   (list, dictionary, string, integer and boolean) for JSON
+   serialization of an object
+"""
+import json
+
+
+def class_to_json(obj):
+    """Returns the dictionary description with simple data
+       structure (list, dictionary, string, integer and
+       boolean) for JSON serialization of an object
+
+       Args:
+            obj: instance of a class
+    """
+    return obj.__dict__
