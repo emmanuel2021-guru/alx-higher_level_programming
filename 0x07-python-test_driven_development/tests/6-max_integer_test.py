6dd8b2bd0fa05f2b62c1cb50d384f05434911848
Emmanuel <emmanuellawakin2005@gmail.com>
2023-02-01 19:07:21 +0100

    Created file and completed task 5

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Unittest for "max_integer" function
+"""
+import unittest
+max_integer = __import__('6-max_integer.py').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """Unittest for "max_integer"
+    """
+    def test_max_integer(self):
+        """Unittest for "max_integer"
+        """
+        self.assertAlmostEqual(max_integer([10, 5, 8, 9, 2]), 10)
+        self.assertAlmostEqual(max_integer([5, 8, 10, 1, 2]), 10)
+        self.assertAlmostEqual(max_integer([1, 8, 5, 3, 10]), 10)
+        self.assertEqual(max_integer([]), None)
