e252c1c9ed5e0e28ea730027fbf24b83f4c639f4
Emmanuel <emmanuellawakin2005@gmail.com>
2023-04-13 20:25:19 +0100

    Completed task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+if (process.argv.length === 3)
+{
+	console.log("Argument found");
+}
+else if (process.argv.length > 3)
+{
+	console.log("Arguments found");
+}
+else
+{
+	console.log("No arguments found");
+}
