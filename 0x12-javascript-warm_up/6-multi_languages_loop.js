8dcf07de12a2211e106753632441155e5a71a4ed
Emmanuel <emmanuellawakin2005@gmail.com>
2023-04-14 02:54:50 +0100

    Fixed code

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
 while (myArray[i]) {
   console.log(myArray[i]);
