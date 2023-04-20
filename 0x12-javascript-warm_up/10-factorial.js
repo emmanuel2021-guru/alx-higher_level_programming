29a714b96438aeaabd685d57a9eb0f6af0451692
Emmanuel <emmanuellawakin2005@gmail.com>
2023-04-20 19:20:28 +0100

    fixed semistandard validation

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
-function factorial(n)
-{
-  if (!n)
-  {
+function factorial (n) {
+  if (!n) {
     return 1;
-  }
-  else
-  {
+  } else {
     return n * factorial(n - 1);
   }
 }
-let fact = factorial(parseInt(process.argv[2]));
+const fact = factorial(parseInt(process.argv[2]));
 console.log(fact);
