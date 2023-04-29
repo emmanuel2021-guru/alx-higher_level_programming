3de694d6ddaa93c2ba9bbec38a4689cfebf987ec
Emmanuel <emmanuellawakin2005@gmail.com>
2023-04-29 11:21:47 +0100

    Fixed semistandard error

--- 0x13-javascript_objects_scopes_closures/7-main.js
+++ 0x13-javascript_objects_scopes_closures/7-main.js
@@ -3,4 +3,4 @@ const nb0ccurences = require('./7-occurrences').nb0ccurences;
 
 console.log(nb0ccurences([1, 2, 3, 4, 5, 6], 8));
 console.log(nb0ccurences([3, 2, 3, 4, 5, 3, 3], 3));
-console.log(nb0ccurences(["S", 12, "c", "S", "School", 8], "S"));
+console.log(nb0ccurences(['S', 12, 'c', 'S', 'School', 8], 'S'));
