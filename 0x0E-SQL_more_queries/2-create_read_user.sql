5870f7066271ab4c11a8b7ac482077573d558bff
Emmanuel <emmanuellawakin2005@gmail.com>
2023-05-01 16:47:08 +0100

    Fixed code

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- This script creates the database hbtn_0d_2 and the user user_0d_2
 CREATE DATABASE hbtn_0d_2;
 CREATE USER 'user_0d_2'@'localhost';
-GRANT SELECT TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
+GRANT SELECT TO 'user_0d_2'@'localhost';
