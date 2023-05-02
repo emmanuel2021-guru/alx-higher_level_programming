a34acbecc39c2e001556075f3bf427155cf426af
Emmanuel <emmanuellawakin2005@gmail.com>
2023-05-02 16:10:50 +0100

    Completed task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- This script creates the database hbtn_0d_2 and the user user_0d_2
 CREATE DATABASE hbtn_0d_2;
-CREATE USER 'user_0d_2'@'localhost';
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT TO 'user_0d_2'@'localhost';
