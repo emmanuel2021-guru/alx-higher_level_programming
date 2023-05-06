3a8936214ce432804574b0ad03cb6a718a6e19a7
Emmanuel <emmanuellawakin2005@gmail.com>
2023-05-06 02:23:29 +0100

    Fixed abbreviation problems

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM cities INNER JOIN states ON c.state_id = states.id ORDER BY cities.id ASC")
+    cur.execute("SELECT * FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
     print(", ".join([ct[2] for ct in cur.fetchall() if ct[4] == sys.argv[4]]))
