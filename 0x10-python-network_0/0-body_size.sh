2470893ed059fd54327118c91e9f9ff92a6fb98d
Emmanuel <emmanuellawakin2005@gmail.com>
2023-05-23 15:13:26 +0100

    Fixed line length

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a request to that URL and displays the size of
-# the body of the response
+# This script takes in a URL, sends a request to that URL and displays the size of the body of the response
 curl -w "%{size_download}\n" -o /dev/null -s "$1"
