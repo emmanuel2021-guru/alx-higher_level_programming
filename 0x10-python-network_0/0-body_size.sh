1842ad07d75aa638b844e08433ec4d09497e5acf
Emmanuel <emmanuellawakin2005@gmail.com>
2023-05-23 15:10:24 +0100

    Fixed script

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # This script takes in a URL, sends a request to that URL and displays the size of
 # the body of the response
-curl -w "%{size_download}\n" -o /dev/null -s $1
+curl -w "%{size_download}\n" -o /dev/null -s "$1"
