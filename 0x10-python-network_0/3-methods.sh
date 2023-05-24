8aca6cd6dd25b3540c0cf5bc8a966453a0c7d633
Emmanuel <emmanuellawakin2005@gmail.com>
2023-05-24 19:12:06 +0100

    Fixed script

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #This script takes in a URL and displays all HTTP methods the server will accept
-curl -sI "$i" | grep "Allow" | cut -d " " -f 2-
+curl -siLX OPTIONS "$i" | grep "Allow:" | cut -b 8-
