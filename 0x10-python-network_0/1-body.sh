c07f7a7ef3c758b52908bcab57dd3867672020f4
Emmanuel <emmanuellawakin2005@gmail.com>
2023-05-24 01:22:05 +0100

    Made curl request silent

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in an URL, sends a GET request to the URL, and displays the body of the response
-re=$(curl -w "%{http_code}\n" -o /dev/null -s "$1") && [ "$re" -eq 200 ] && curl "$1"
+re=$(curl -w "%{http_code}\n" -o /dev/null -s "$1") && [ "$re" -eq 200 ] && curl -s "$1"
