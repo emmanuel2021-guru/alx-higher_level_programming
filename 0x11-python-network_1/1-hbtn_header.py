fda26c455fe4c8e874a96aa2fe3018b4e8570f15
Emmanuel <emmanuellawakin2005@gmail.com>
2023-05-27 05:06:58 +0100

    Used get to access dictionary

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -10,4 +10,4 @@ import sys
 
 if __name__ == "__main__":
     with urllib.request.urlopen(sys.argv[1]) as response:
-        print(response.headers['X-Request-Id'])
+        print(response.headers.get('X-Request-Id'))
