bafba41fa8e8afd1f6350d9220347d95014171e8
Emmanuel <emmanuellawakin2005@gmail.com>
2023-05-30 15:32:07 +0100

    Completed task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""This script takes in a URL, sends a request to the URL and displays
+the body of the response (decoded in utf-8)"""
+
+
+import urllib.request
+import urllib.error
+import sys
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            html = response.read()
+            print("{}".format(html.decode('utf-8')))
+    except urllib.error.HTTPError as e:
+        print("Error code: ".format(e.code))
