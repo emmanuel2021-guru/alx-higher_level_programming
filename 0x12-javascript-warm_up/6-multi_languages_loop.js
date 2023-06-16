88edb950ee3820d0e4c677baaf0e82ab25910d38
Emmanuel <emmanuellawakin2005@gmail.com>
2023-06-16 17:07:48 +0100

    Removed var from code

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-var arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-var i = 0;
+const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let i = 0;
 while (arr[i]) {
   console.log(arr[i]);
   i++;
