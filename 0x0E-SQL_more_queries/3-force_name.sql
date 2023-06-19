569be8e7d12a097f3540c5564aed5bbb16003d5d
Emmanuel <emmanuellawakin2005@gmail.com>
2023-06-19 17:06:08 +0100

    Fixed syntax of code

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
 -- This script creates the table 'force_name' on a MySQL server
-CREATE TABLE IF NOT EXISTS 'force_name' ('id' INT, 'name' VARCHAR(256));
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256));
