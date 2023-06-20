9405984726e39dce26e8c239fc1b9432dd536f3b
Emmanuel <emmanuellawakin2005@gmail.com>
2023-06-20 01:28:58 +0100

    Added some lines of code to change the database working on

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,4 @@
 -- This script creates the database 'hbtn_0d_usa' and the table 'states' (in 'hbtn_0d_usa') on a MySQL server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
