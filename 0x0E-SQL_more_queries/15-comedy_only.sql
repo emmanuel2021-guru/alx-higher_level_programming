6bb62b4bff221f7ef02cac2a3ecddfe1e1dcf0da
Emmanuel <emmanuellawakin2005@gmail.com>
2023-06-23 02:22:25 +0100

    Completed task 15

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,2 @@
+-- This script lists all comedy shows in the database 'hbtn_0d_tvshows'
+SELECT ts.title FROM tv_genres tg INNER JOIN tv_show_genres tsg ON tg.id = tsg.genre_id INNER JOIN tv_shows ts ON tsg.show_id = ts.id WHERE tg.name = 'Comedy' ORDER BY ts.title;
