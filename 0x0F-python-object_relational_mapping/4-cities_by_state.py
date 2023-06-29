83999c0e33cf1630ac547da2ac5b6d9cafbf4c36
Emmanuel <emmanuellawakin2005@gmail.com>
2023-06-29 06:02:56 +0100

    Completed task 5

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
-                LEFT OUTER JOIN states ON cities.state_id = states.id\
+                INNER JOIN states ON cities.state_id = states.id\
                 ORDER BY cities.id")
     rows = cur.fetchall()
     for row in rows:
