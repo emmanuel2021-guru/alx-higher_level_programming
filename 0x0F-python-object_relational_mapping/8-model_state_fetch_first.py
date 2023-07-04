5dbdb09e6d53695d4b32d038cab4de96343fa05a
Emmanuel <emmanuellawakin2005@gmail.com>
2023-07-04 13:15:14 +0100

    Completed task 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""This script prints the first 'State' object
+from the database 'hbtn_0e_6_usa'"""
+
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import sys
+
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).filter_by(id='1'):
+        print("{}: {}".format(instance.id, instance.name))
