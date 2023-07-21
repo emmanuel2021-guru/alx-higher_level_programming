2e053da0d1f11c5231c16530a4898be816758a96
Emmanuel <emmanuellawakin2005@gmail.com>
2023-07-21 03:54:56 +0100

    Fixed code

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -15,9 +15,9 @@ if __name__ == "__main__":
     req = requests.post('http://0.0.0.0:5000/search_user', data=p_data)
     try:
         json_r = req.json()
+        if json_r == "":
+            print("No result")
+        else:
+            print("[{}] {}".format(json_r['id'], json_r['name']))
     except JSONDecodeError:
         print("Not a valid JSON")
-    if json_r == "":
-        print("No result")
-    else:
-        print("[{}] {}".format(json_r['id'], json_r['name']))
