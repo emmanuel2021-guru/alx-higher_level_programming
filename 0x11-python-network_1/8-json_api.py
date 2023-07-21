9653fe5527c3977419722d43261dcc838c223e0b
Emmanuel <emmanuellawakin2005@gmail.com>
2023-07-21 03:23:08 +0100

    Completed task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""This script takes in a letter and sends a POST request to 
+'http://0.0.0.0:5000/search_user' with the letter as a parameter"""
+
+import requests, sys
+
+
+if __name__ == "__main__":
+    if sys.argv[1] == None:
+        p_data = {'q': ""}
+    else:
+        p_data = {'q': sys.argv[1]}
+    req = requests.post('http://0.0.0.0:5000/search_user', data=p_data)
+    try:
+        if req.json() == "":
+            print("No result")
+        else:
+            print(req.json())
+    except JSONDecodeError:
+        print("Not a valid JSON")
