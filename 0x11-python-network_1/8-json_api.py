1bd4d3bce06ed68ab02f1ce518bc59f60f2d0559
Emmanuel <emmanuellawakin2005@gmail.com>
2023-07-21 04:00:56 +0100

    Fixed exception

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -19,5 +19,5 @@ if __name__ == "__main__":
             print("No result")
         else:
             print("[{}] {}".format(json_r['id'], json_r['name']))
-    except requests.exceptions.JSONDecodeError:
+    except json.decoder.JSONDecodeError:
         print("Not a valid JSON")
