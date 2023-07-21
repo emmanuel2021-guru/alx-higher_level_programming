44d93c4626cdb67e8f12de35dd3e49e3438419c2
Emmanuel <emmanuellawakin2005@gmail.com>
2023-07-21 03:50:38 +0100

    Fixed code for when q is "" and when q is not a valid JSON

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""This script takes in a letter and sends a POST request to 
+"""This script takes in a letter and sends a POST request to
 'http://0.0.0.0:5000/search_user' with the letter as a parameter"""
 
 import requests
@@ -8,16 +8,16 @@ import sys
 
 
 if __name__ == "__main__":
-    if sys.argv[1] is None:
+    if len(sys.argv) == 1:
         p_data = {'q': ""}
     else:
         p_data = {'q': sys.argv[1]}
     req = requests.post('http://0.0.0.0:5000/search_user', data=p_data)
     try:
         json_r = req.json()
-        if json_r == "":
-            print("No result")
-        else:
-            print("[{}] {}".format(json_r['id'], json_r['name']))
     except JSONDecodeError:
         print("Not a valid JSON")
+    if json_r == "":
+        print("No result")
+    else:
+        print("[{}] {}".format(json_r['id'], json_r['name']))
