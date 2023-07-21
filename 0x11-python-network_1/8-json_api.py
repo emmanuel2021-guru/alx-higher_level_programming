a274c16a2e7c6d811a7d33b5abb73100ec0e51fb
Emmanuel <emmanuellawakin2005@gmail.com>
2023-07-21 03:25:42 +0100

    Changed the import statements

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -3,7 +3,8 @@
 """This script takes in a letter and sends a POST request to 
 'http://0.0.0.0:5000/search_user' with the letter as a parameter"""
 
-import requests, sys
+import requests
+import sys
 
 
 if __name__ == "__main__":
