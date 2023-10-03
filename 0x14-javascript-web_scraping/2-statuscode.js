f211be177331645f0f63d1be81e489159383f8c6
Emmanuel <emmanuellawakin2005@gmail.com>
2023-10-03 16:44:02 +0100

    Fixed code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
 request(process.argv[2], (error, response, body) => {
-  console.log("code: " + response.statusCode)
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
 });
