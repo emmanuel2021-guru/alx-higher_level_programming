25e3b2ee231a30865fe8bfba1389f8b79b7d81f3
Emmanuel <emmanuellawakin2005@gmail.com>
2023-10-04 03:20:40 +0100

    Completed task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+request(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    fs.writeFile(process.argv[3], body, 'utf8', (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
+});
